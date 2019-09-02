a53ca37a2f074fb17820688873e96074531624e0
malasip <alasippolamika@gmail.com>
2019-09-02 17:09:54 +0300

    Initial commit

---
+++ src/main/java/com/simpledevicedatabase/simpleddb/SimpleDeviceDatabase.java
@@ -0,0 +1,13 @@
+package com.simpledevicedatabase.simpleddb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDeviceDatabase {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDeviceDatabase.class, args);
+	}
+
+}
---
+++ src/main/resources/static/js/dashboard.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    deviceTable = $('#deviceList').DataTable({
+        searching: true,
+        stateSave: true,
+        dom: 'lrtip',
+        scrollY: '100%'
+    });
+    $("#search").on("keyup", function() {
+        deviceTable.search($(this).val()).draw();
+    });
+    $('#addDeviceModal').on('show.bs.modal', function (event) {
+        var modal = $(this)
+        modal.find('.modal-body').load("/dashboard/addDeviceForm");
+    });
+    $('#deviceDetailModal').on('show.bs.modal', function (event) {
+        var modal = $(this)
+        var button = $(event.relatedTarget)
+        var id = button.data('device_id')
+        modal.find('.modal-body').load("/device/showDeviceDetail/" + id);
+    });
+});
