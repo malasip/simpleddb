d9cd2cfbd626cd95ac8b4580208a3864484ec80d
malasip <alasippolamika@gmail.com>
2019-10-17 14:49:27 +0300

    Update

--- src/main/java/com/simpledevicedatabase/simpleddb/SimpleDeviceDatabase.java
+++ src/main/java/com/simpledevicedatabase/simpleddb/SimpleDeviceDatabase.java
@@ -22,7 +22,7 @@ public class SimpleDeviceDatabase {
 		SpringApplication.run(SimpleDeviceDatabase.class, args);
 	}
 
-	/*@Bean
+	@Bean
 	public CommandLineRunner deviceDemo(
 		UserRepository urepository,
 		UserRoleRepository urrepository,
@@ -32,7 +32,7 @@ public class SimpleDeviceDatabase {
 		return (args) -> {
 			urrepository.save(new UserRole("USER"));
 			urrepository.save(new UserRole("ADMIN"));
-			/*urepository.save(new User("admin",
+			urepository.save(new User("admin",
 				"$2a$10$Hg1Lg3TyNHngcHbuUup/4uiXbu7BVd9bERV4W5MgHiYjzM7cJq8eC",
 				"admin@local",
 				urrepository.findByName("ADMIN"),
@@ -42,5 +42,5 @@ public class SimpleDeviceDatabase {
 			dmrepository.save(new DeviceModel("OptiPlex 3060"));
 			dmrepository.save(new DeviceModel("EliteBook 840 G6"));
 		};
-	}*/
+	}
 }
--- src/main/java/com/simpledevicedatabase/simpleddb/domain/UserRepository.java
+++ src/main/java/com/simpledevicedatabase/simpleddb/domain/UserRepository.java
@@ -1,15 +1,8 @@
 package com.simpledevicedatabase.simpleddb.domain;
 
 import org.springframework.data.repository.CrudRepository;
-//import org.springframework.data.repository.Repository;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    
-    //@Override
-    //@PreAuthorize("hasAuthority('ADMIN') or #username == authentication.principal.username")
-    //User save(User user);
 
     User findByUsername(String username);
 }
